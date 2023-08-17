8ab5d6e029d4e29ed673dc41eb1b9bb6ed4f7c2b
Sushil97 <sushil500530@gmail.com>
2023-08-17 22:20:45 +0600

    solve ellipse Area math

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -26,3 +26,32 @@ function calculatorTriangleArea(){
         ractangle.innerText = ractangleArea
     }
 
+    function calculateParallelogramArea(){
+        const base = getInputValue('parallelogram-base')
+         const height = getInputValue('parallelogram-height')   
+         const area = base * height ;
+        //  const paralleArea = document.getElementById('parallelogram') 
+        //  paralleArea.innerText = area ;
+         setElementInnerText('parallelogram',area)
+     }
+     // reusable function 
+ 
+     function getInputValue(fieldId){
+         const inputField = document.getElementById(fieldId).value ;
+         const value = parseFloat(inputField) ;
+         return value ;
+     }
+     // reusable set span,text,p, div, etc value of function 
+     function setElementInnerText(elementId, area){
+         const element = document.getElementById(elementId) 
+         element.innerText = area ;
+     }
+
+    //  ellipse area 
+    function calculateEllipseArea(){
+        const ellipseMajor = getInputValue('Ellipse-a')
+        const ellipseMinor = getInputValue('Ellipse-b') 
+        const area = 3.1416 * ellipseMajor * ellipseMinor ;
+        // const area = Math.PI*(ellipseMajor * ellipseMinor)
+        setElementInnerText('Ellipse', area)
+    }
