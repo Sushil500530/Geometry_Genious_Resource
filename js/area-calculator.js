713f055edf1753606cc330fc92499abe9ff9e09f
Sushil97 <sushil500530@gmail.com>
2023-08-18 08:33:16 +0600

    validation and area's details entry show

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -8,6 +8,11 @@ function calculatorTriangleArea(){
         const inputHeightElement = document.getElementById('input-height').value ;
         const inputHeight = parseFloat(inputHeightElement) 
         // console.log(inputHeight)
+        // validation 
+        if(isNaN(inputBase) || isNaN(inputHeight)){
+            alert('please insert a number...!') 
+            return ;
+        }
     
         // calculate area 
         const triangleArea = 0.5 * inputBase * inputHeight ;
@@ -18,20 +23,36 @@ function calculatorTriangleArea(){
     }
     function calculateRactanglerArea(){
         const inputRactangleBase = document.getElementById('ractangle-base').value ;
-        const base = parseFloat(inputRactangleBase) ;
+        const width = parseFloat(inputRactangleBase) ;
         const inputRactangleHeight = document.getElementById('ractangle-height').value ;
-        const height = parseFloat(inputRactangleHeight) 
-        const ractangleArea = base * height ;
+        const length = parseFloat(inputRactangleHeight) 
+        // valided input: width and length 
+        if(isNaN(width) || isNaN(length)){
+           alert('please insert a number...!')
+           return ;
+        }
+
+        const ractangleArea = width * length ;
         const ractangle = document.getElementById('rectangle')
         ractangle.innerText = ractangleArea
     }
 
     function calculateParallelogramArea(){
         const base = getInputValue('parallelogram-base')
-         const height = getInputValue('parallelogram-height')   
-         const area = base * height ;
-        //  const paralleArea = document.getElementById('parallelogram') 
+         const height = getInputValue('parallelogram-height')
+         
+        //  validation 
+        if(isNaN(base) || isNaN(height)){
+            alert('please insert a number...!') 
+            return ;
+        }
+        const area = base * height ;
         //  paralleArea.innerText = area ;
+        //  const paralleArea = document.getElementById('parallelogram') 
+
+        // add to calculation entry 
+        addCalculatioEntry('perallelogram', area)
+
          setElementInnerText('parallelogram',area)
      }
      // reusable function 
@@ -51,9 +72,50 @@ function calculatorTriangleArea(){
     function calculateEllipseArea(){
         const ellipseMajor = getInputValue('Ellipse-a')
         const ellipseMinor = getInputValue('Ellipse-b') 
+        // validation 
+        if(isNaN(ellipseMajor) || isNaN(ellipseMinor)){
+            alert('please insert a number...!') 
+            return ;
+        }
         const area = 3.1416 * ellipseMajor * ellipseMinor ;
         const areaResult = area.toFixed(2)
         // const area = Math.PI*(ellipseMajor * ellipseMinor)
         setElementInnerText('Ellipse', areaResult)
 
     }
+
+    // add to calculation entry 
+    /*
+    1.get the element where you want to added the dynamic HTML
+    2.create an element you want to add 
+    3.if you need some class 
+    4.set inner HTML,it could be dynamic template string
+    5.append the created element as a child of the parent 
+    */
+    function addCalculatioEntry(areaType , area){
+        console.log(areaType + ' ' + area)
+        const calculationEntry = document.getElementById('calculation-details')
+
+        // koyta element ace seta dekhar jonno 
+        const count = calculationEntry.childElementCount ;
+        
+        const p = document.createElement('p') ;
+        p.classList.add('my-4')
+        p.innerHTML = `${count + 1} ${areaType} ${area} cm<sup>2</sup> <button class ="btn btn-sm btn-success" id="btn-delete">Convert</button>`;
+        // p.innerHTML = areaType + ' ' + area ;
+        calculationEntry.appendChild(p)
+        // document.getElementById('btn-delete').addEventListener('click',function(event){
+        //     event.parentNode.childNodes 
+        //     console.log(event)
+        // })
+        //const btn = document.getElementById('btn-delete')
+        // btn.target.p
+    }
+    
+    // data validation 
+    /*
+    1.set the proper type of the input field( number,email,data,emeil) 
+    2.check type using typeof 
+    3.isNaN-(Not a Number)...checking number
+
+    */
